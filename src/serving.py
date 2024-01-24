720db2c4bffd502c14d757e1c54dfc6bd4cb1e08
Avi Asulin <avia@iguazio.com>
2024-01-24 10:24:31 +0200

    add serving code

---
+++ src/serving.py
@@ -0,0 +1,60 @@
+import mlrun
+from cloudpickle import load
+from typing import List
+import xgboost as xgb
+import tarfile
+import numpy as np
+
+import warnings
+
+warnings.filterwarnings("ignore")
+
+
+class XGBModelServer(mlrun.serving.V2ModelServer):
+    def load(self):
+        """load and initialize the model and/or other elements"""
+        
+        # Download the model file:
+        model_file, extra_data = self.get_model(".tar.gz")
+        
+        # Extract model file:
+        t = tarfile.open(model_file, 'r:gz')
+        t.extractall()
+        
+        # Load model from file:
+        self.model = load(open("xgboost-model", "rb"))
+
+    def predict(self, body: dict) -> List:
+        """Generate model predictions from sample."""
+        
+        # Convert input to numpy array:
+        data = np.asarray(body["inputs"])
+        
+        # Transform into XGBoost object:
+        data = xgb.DMatrix(data)
+        
+        # Perform prediction:
+        result: np.ndarray = self.model.predict(data)
+        return result.tolist()
+
+
+def postprocess(inputs: dict) -> dict:
+    """
+    Postprocessing the output of the model
+    """
+    # Read the prediction:
+    print(inputs)
+    outputs = np.asarray(inputs.pop("outputs"))
+    predictions = []
+    confidences = []
+
+    # Turn predictions into categories and extract confidence:
+    for prediction in outputs:
+        pred = prediction.argmax()
+        confidence = max(prediction)
+        predictions.extend([int(pred)])
+        confidences.extend([float(confidence)])
+        
+    inputs["predictions"] = predictions
+    inputs["confidences"] = confidences
+    return inputs
